25cbbf8d984d224ef545215dfdfd197299c8a347
Antony Trinh <antony.trinh@ni.com>
2016-09-26 16:18:47 -0500

    strcmp

---
+++ exercises/StrCmp.h
@@ -0,0 +1,18 @@
+int my_strcmp(const char* first, const char* second) {
+	int index = 0;
+	while(first[index] != '\0' || second[index] != '\0') {
+		if(first[index] != second[index]) {
+			return first[index] - second[index];
+		}
+		++index;
+	}
+	if(first[index] == '\0' && second[index] == '\0') {
+		return 0;
+	}
+	if(first[index] == '\0') {
+		return -second[index];
+	}
+	if(second[index] == '\0') {
+		return -first[index];
+	}
+}

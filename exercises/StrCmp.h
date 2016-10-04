99cb3717ec39c741946ef6ce356a5d88aab06f90
Antony <anttrinh8012@gmail.com>
2016-10-03 21:20:16 -0500

    merging

--- exercises/StrCmp.h
+++ exercises/StrCmp.h
@@ -1,26 +1,14 @@
+// --------
+// StrCmp.h
+// --------
+
 #ifndef StrCmp_h
 #define StrCmp_h
 
-int my_strcmp(const char* first, const char* second) {
-	int index = 0;
-	while(first[index] != '\0' || second[index] != '\0') {
-		if(first[index] != second[index]) {
-			return first[index] - second[index];
-		}
-		++index;
-	}
-	if(first[index] == '\0' && second[index] == '\0') {
-		return 0;
-	}
-	if(first[index] == '\0') {
-		return -second[index];
-	}
-	if(second[index] == '\0') {
-		return -first[index];
-	}
-	else {
-		return -1;
-	}
-}
+int my_strcmp (const char* a, const char* b) {
+    while ((*a != 0) && (*a == *b)) {
+        ++a;
+        ++b;}
+    return *a - *b;}
 
-#endif
+#endif // StrCmp_h
